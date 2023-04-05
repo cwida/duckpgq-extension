70ccbadee069c32468075be202fc541b9fa13fff
dtenwolde <dljtw98@gmail.com>
2023-04-05 09:57:01 +0200

    Changed extension name to sqlpgq

---
+++ test/nodejs/sqlpgq_test.js
@@ -0,0 +1,20 @@
+var duckdb = require('../../duckdb/tools/nodejs');
+var assert = require('assert');
+
+describe(`sqlpgq extension`, () => {
+    let db;
+    let conn;
+    before((done) => {
+        db = new duckdb.Database(':memory:');
+        conn = new duckdb.Connection(db);
+        done();
+    });
+
+    it('function should return expected constant', function (done) {
+        db.all("SELECT sqlpgq('Sam') as value;", function (err, res) {
+            if (err) throw err;
+            assert.deepEqual(res, [{value: "Sqlpgq Sam 🐥"}]);
+            done();
+        });
+    });
+});
