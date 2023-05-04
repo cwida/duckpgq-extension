acdd2b685f617ede8c4ee5985ea5f2cb47d7418d
dtenwolde <dljtw98@gmail.com>
2023-05-04 11:51:43 +0200

    initial commit

--- src/duckpgq_extension.cpp
+++ src/duckpgq_extension.cpp
@@ -1,6 +1,6 @@
 #define DUCKDB_EXTENSION_MAIN
 
-#include "quack_extension.hpp"
+#include "duckpgq_extension.hpp"
 #include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/string_util.hpp"
@@ -11,12 +11,12 @@
 
 namespace duckdb {
 
-inline void QuackScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+inline void DuckpgqScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
     auto &name_vector = args.data[0];
     UnaryExecutor::Execute<string_t, string_t>(
 	    name_vector, result, args.size(),
 	    [&](string_t name) { 
-			return StringVector::AddString(result, "Quack "+name.GetString()+" 🐥");;
+			return StringVector::AddString(result, "Duckpgq "+name.GetString()+" 🐥");;
         });
 }
 
@@ -26,29 +26,29 @@ static void LoadInternal(DatabaseInstance &instance) {
 
     auto &catalog = Catalog::GetSystemCatalog(*con.context);
 
-    CreateScalarFunctionInfo quack_fun_info(
-            ScalarFunction("quack", {LogicalType::VARCHAR}, LogicalType::VARCHAR, QuackScalarFun));
-    quack_fun_info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
-    catalog.CreateFunction(*con.context, &quack_fun_info);
+    CreateScalarFunctionInfo duckpgq_fun_info(
+            ScalarFunction("duckpgq", {LogicalType::VARCHAR}, LogicalType::VARCHAR, DuckpgqScalarFun));
+    duckpgq_fun_info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
+    catalog.CreateFunction(*con.context, duckpgq_fun_info);
     con.Commit();
 }
 
-void QuackExtension::Load(DuckDB &db) {
+void DuckpgqExtension::Load(DuckDB &db) {
 	LoadInternal(*db.instance);
 }
-std::string QuackExtension::Name() {
-	return "quack";
+std::string DuckpgqExtension::Name() {
+	return "duckpgq";
 }
 
 } // namespace duckdb
 
 extern "C" {
 
-DUCKDB_EXTENSION_API void quack_init(duckdb::DatabaseInstance &db) {
+DUCKDB_EXTENSION_API void duckpgq_init(duckdb::DatabaseInstance &db) {
 	LoadInternal(db);
 }
 
-DUCKDB_EXTENSION_API const char *quack_version() {
+DUCKDB_EXTENSION_API const char *duckpgq_version() {
 	return duckdb::DuckDB::LibraryVersion();
 }
 }
--- test/nodejs/duckpgq_test.js
+++ test/nodejs/duckpgq_test.js
@@ -1,7 +1,7 @@
 var duckdb = require('../../duckdb/tools/nodejs');
 var assert = require('assert');
 
-describe(`quack extension`, () => {
+describe(`duckpgq extension`, () => {
     let db;
     let conn;
     before((done) => {
@@ -11,9 +11,9 @@ describe(`quack extension`, () => {
     });
 
     it('function should return expected constant', function (done) {
-        db.all("SELECT quack('Sam') as value;", function (err, res) {
+        db.all("SELECT duckpgq('Sam') as value;", function (err, res) {
             if (err) throw err;
-            assert.deepEqual(res, [{value: "Quack Sam 🐥"}]);
+            assert.deepEqual(res, [{value: "Duckpgq Sam 🐥"}]);
             done();
         });
     });
