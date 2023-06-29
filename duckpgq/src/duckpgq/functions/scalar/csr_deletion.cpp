f43ab16deedcced1f29e44c048bf7a25b922f2e0
dtenwolde <dljtw98@gmail.com>
2023-06-29 16:01:43 +0200

    Adding UDFs back in

---
+++ duckpgq/src/duckpgq/functions/scalar/csr_deletion.cpp
@@ -0,0 +1,35 @@
+#include "duckdb/main/client_data.hpp"
+#include "duckdb/planner/expression/bound_function_expression.hpp"
+#include "duckpgq/common.hpp"
+#include "duckpgq/duckpgq_functions.hpp"
+#include <duckpgq_extension.hpp>
+
+namespace duckdb {
+
+static void DeleteCsrFunction(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto &func_expr = (BoundFunctionExpression &)state.expr;
+	auto &info = (CSRFunctionData &)*func_expr.bind_info;
+
+	auto sqlpgq_state_entry = info.context.registered_state.find("sqlpgq");
+	if (sqlpgq_state_entry == info.context.registered_state.end()) {
+		//! Wondering how you can get here if the extension wasn't loaded, but leaving this check in anyways
+		throw MissingExtensionException("The DuckPGQ extension has not been loaded");
+	}
+	auto duckpgq_state = reinterpret_cast<DuckPGQState *>(sqlpgq_state_entry->second.get());
+
+	int flag = duckpgq_state->csr_list.erase(info.id);
+	result.SetVectorType(VectorType::CONSTANT_VECTOR);
+	auto result_data = ConstantVector::GetData<bool>(result);
+	result_data[0] = (flag == 1);
+}
+
+CreateScalarFunctionInfo DuckPGQFunctions::GetDeleteCsrFunction() {
+	ScalarFunctionSet set("delete_csr");
+
+	set.AddFunction(ScalarFunction("delete_csr", {LogicalType::INTEGER}, LogicalType::BOOLEAN, DeleteCsrFunction,
+	                               CSRFunctionData::CSRBind));
+
+	return CreateScalarFunctionInfo(set);
+}
+
+} // namespace duckdb
---
+++ duckpgq/src/duckpgq/functions/scalar/csr_get_w_type.cpp
@@ -0,0 +1,51 @@
+#include <duckpgq_extension.hpp>
+#include "duckdb/main/client_data.hpp"
+#include "duckdb/planner/expression/bound_function_expression.hpp"
+#include "duckpgq/common.hpp"
+#include "duckpgq/duckpgq_functions.hpp"
+
+namespace duckdb {
+
+enum class CSRWType : int32_t {
+	// possible weight types of a csr
+	UNWEIGHTED,   //! unweighted
+	INTWEIGHT,    //! integer
+	DOUBLEWEIGHT, //! double
+};
+
+static void GetCsrWTypeFunction(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto &func_expr = (BoundFunctionExpression &)state.expr;
+	auto &info = (CSRFunctionData &)*func_expr.bind_info;
+
+	auto sqlpgq_state_entry = info.context.registered_state.find("sqlpgq");
+	if (sqlpgq_state_entry == info.context.registered_state.end()) {
+		//! Wondering how you can get here if the extension wasn't loaded, but leaving this check in anyways
+		throw MissingExtensionException("The SQL/PGQ extension has not been loaded");
+	}
+	auto sqlpgq_state = reinterpret_cast<DuckPGQState *>(sqlpgq_state_entry->second.get());
+	result.SetVectorType(VectorType::CONSTANT_VECTOR);
+	auto result_data = ConstantVector::GetData<int32_t>(result);
+	auto csr = sqlpgq_state->GetCSR(info.id);
+	int32_t flag;
+	if (!csr->initialized_w) {
+		flag = (int32_t)CSRWType::UNWEIGHTED;
+	} else if (csr->w.size()) {
+		flag = (int32_t)CSRWType::INTWEIGHT;
+	} else if (csr->w_double.size()) {
+		flag = (int32_t)CSRWType::DOUBLEWEIGHT;
+	} else {
+		throw InternalException("Corrupted weight vector");
+	}
+	result_data[0] = flag;
+}
+
+CreateScalarFunctionInfo DuckPGQFunctions::GetGetCsrWTypeFunction() {
+	ScalarFunctionSet set("sqlpgq_csr_get_w_type");
+
+	set.AddFunction(ScalarFunction("sqlpgq_csr_get_w_type", {LogicalType::INTEGER}, LogicalType::INTEGER,
+	                               GetCsrWTypeFunction, CSRFunctionData::CSRBind));
+
+	return CreateScalarFunctionInfo(set);
+}
+
+} // namespace duckdb
