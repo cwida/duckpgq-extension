3374e767cb64abcdd3e8aaf13a36813adc1df41b
dtenwolde <dljtw98@gmail.com>
2023-07-05 11:45:41 +0200

    Adding release and debug options

--- scripts/copy_tests.py
+++ scripts/copy_tests.py
@@ -4,8 +4,25 @@ from os.path import isfile, join, exists
 from pathlib import Path
 import shutil
 from textwrap import dedent
+import sys
+import getopt
+import os
 
-def main():
+
+def main(argv):
+    mode = ''
+    opts, args = getopt.getopt(argv, "hm:", ["mode=", "ofile="])
+    for opt, arg in opts:
+        if opt == '-h':
+            print('copy_tests.py -m <release/debug>')
+            sys.exit()
+        elif opt in ("-m", "--mode"):
+            inputfile = arg
+    if mode != "release" or mode != "debug":
+        raise Exception("Invalid parameter, --mode should be release or debug")
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
     test_path_duckpgq = Path("../test/sql")
     test_path_duckdb = Path("../duckdb/test/extension/duckpgq")
 
@@ -21,19 +38,18 @@ def main():
         f = open(test_path_duckpgq / file, "r")
         content = f.read()
         content = content.replace("require duckpgq\n",
-                                  dedent("""\
+                                  dedent(f"""\
                                   statement ok
-                                  install '__BUILD_DIR__/../../../build/release/extension/duckpgq/duckpgq.duckdb_extension';
+                                  install '__BUILD_DIR__/../../../build/{mode}/extension/duckpgq/duckpgq.duckdb_extension';
                                   
                                   statement ok
                                   load 'duckpgq';
                                   """))
 
-        new_file = open(test_path_duckdb/file, "w")
+        new_file = open(test_path_duckdb / file, "w")
         new_file.write(content)
         new_file.close()
 
 
 if __name__ == "__main__":
-    main()
-
+    main(sys.argv[1:])
