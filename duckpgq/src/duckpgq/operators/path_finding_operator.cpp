57996010350a32a2855086936944d26ebf566f33
dtenwolde <dljtw98@gmail.com>
2024-02-15 12:07:48 +0100

    Format

--- duckpgq/src/duckpgq/operators/path_finding_operator.cpp
+++ duckpgq/src/duckpgq/operators/path_finding_operator.cpp
@@ -1,18 +1,20 @@
 
 #include "duckpgq/operators/path_finding_operator.hpp"
-#include <duckpgq_extension.hpp>
 #include <duckpgq/operators/physical_path_finding.hpp>
+#include <duckpgq_extension.hpp>
 
 namespace duckdb {
-	unique_ptr<PhysicalOperator> PathFindingOperator::CreatePlan(ClientContext &,
-																												duckdb::PhysicalPlanGenerator &generator) {
-		D_ASSERT(children.size() == 2);
-		auto left = generator.CreatePlan(std::move(children[0]));
-		auto right = generator.CreatePlan(std::move(children[1]));
+unique_ptr<PhysicalOperator>
+PathFindingOperator::CreatePlan(ClientContext &,
+                                duckdb::PhysicalPlanGenerator &generator) {
+  D_ASSERT(children.size() == 2);
+  auto left = generator.CreatePlan(std::move(children[0]));
+  auto right = generator.CreatePlan(std::move(children[1]));
 
-		auto result = duckdb::make_uniq_base<duckdb::PhysicalOperator, PhysicalPathFinding>(*this, std::move(left), std::move(right));
-		result->children.emplace_back(std::move(result));
-		return result;
-	}
-
+  auto result =
+      duckdb::make_uniq_base<duckdb::PhysicalOperator, PhysicalPathFinding>(
+          *this, std::move(left), std::move(right));
+  result->children.emplace_back(std::move(result));
+  return result;
 }
+} // namespace duckdb
