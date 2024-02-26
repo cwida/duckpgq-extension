a46ff6e2eccc9a5e816703d12be41975951c213b
dtenwolde <dljtw98@gmail.com>
2024-02-26 16:59:46 +0100

    Create plan

--- duckpgq/src/duckpgq/operators/path_finding_operator.cpp
+++ duckpgq/src/duckpgq/operators/path_finding_operator.cpp
@@ -7,13 +7,12 @@ namespace duckdb {
 unique_ptr<PhysicalOperator>
 PathFindingOperator::CreatePlan(ClientContext &,
                                 duckdb::PhysicalPlanGenerator &generator) {
-  // D_ASSERT(children.size() == 2);
-  // auto left = generator.CreatePlan(std::move(children[0]));
-  // auto right = generator.CreatePlan(std::move(children[1]));
-  //
-  // auto result =
-  //     duckdb::make_uniq_base<duckdb::PhysicalOperator, PhysicalPathFinding>(*this);
-  // result->children.emplace_back(std::move(result));
-  // return result;
+   D_ASSERT(children.size() == 2);
+   auto left = generator.CreatePlan(std::move(children[0]));
+   auto right = generator.CreatePlan(std::move(children[1]));
+   return make_uniq<PhysicalPathFinding>(*this, std::move(left), std::move(right));
+}
+vector<ColumnBinding> PathFindingOperator::GetColumnBindings() {
+  return LogicalOperator::GetColumnBindings();
 }
 } // namespace duckdb
