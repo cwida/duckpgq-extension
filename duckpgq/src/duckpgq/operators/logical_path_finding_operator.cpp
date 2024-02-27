b1630b087091883aea40fde3fccc81a169e9cb74
dtenwolde <dljtw98@gmail.com>
2024-02-27 10:40:44 +0100

    Format fix

--- duckpgq/src/duckpgq/operators/logical_path_finding_operator.cpp
+++ duckpgq/src/duckpgq/operators/logical_path_finding_operator.cpp
@@ -4,13 +4,13 @@
 #include <duckpgq_extension.hpp>
 
 namespace duckdb {
-unique_ptr<PhysicalOperator>
-LogicalPathFindingOperator::CreatePlan(ClientContext &,
-                                duckdb::PhysicalPlanGenerator &generator) {
-   D_ASSERT(children.size() == 2);
-   auto left = generator.CreatePlan(std::move(children[0]));
-   auto right = generator.CreatePlan(std::move(children[1]));
-   return make_uniq<PhysicalPathFinding>(*this, std::move(left), std::move(right));
+unique_ptr<PhysicalOperator> LogicalPathFindingOperator::CreatePlan(
+    ClientContext &, duckdb::PhysicalPlanGenerator &generator) {
+  D_ASSERT(children.size() == 2);
+  auto left = generator.CreatePlan(std::move(children[0]));
+  auto right = generator.CreatePlan(std::move(children[1]));
+  return make_uniq<PhysicalPathFinding>(*this, std::move(left),
+                                        std::move(right));
 }
 vector<ColumnBinding> LogicalPathFindingOperator::GetColumnBindings() {
   return LogicalOperator::GetColumnBindings();
