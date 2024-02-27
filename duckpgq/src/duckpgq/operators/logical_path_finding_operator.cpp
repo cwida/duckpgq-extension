6d7382c2ef6abaa19787c78ac3aacd3530cbe6f9
dtenwolde <dljtw98@gmail.com>
2024-02-27 10:28:31 +0100

    Rename

--- duckpgq/src/duckpgq/operators/logical_path_finding_operator.cpp
+++ duckpgq/src/duckpgq/operators/logical_path_finding_operator.cpp
@@ -5,14 +5,14 @@
 
 namespace duckdb {
 unique_ptr<PhysicalOperator>
-PathFindingOperator::CreatePlan(ClientContext &,
+LogicalPathFindingOperator::CreatePlan(ClientContext &,
                                 duckdb::PhysicalPlanGenerator &generator) {
    D_ASSERT(children.size() == 2);
    auto left = generator.CreatePlan(std::move(children[0]));
    auto right = generator.CreatePlan(std::move(children[1]));
    return make_uniq<PhysicalPathFinding>(*this, std::move(left), std::move(right));
 }
-vector<ColumnBinding> PathFindingOperator::GetColumnBindings() {
+vector<ColumnBinding> LogicalPathFindingOperator::GetColumnBindings() {
   return LogicalOperator::GetColumnBindings();
 }
 } // namespace duckdb
