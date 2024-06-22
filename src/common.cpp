e360f0fa60004f72a78e9d4322fb75dd7e028d94
dtenwolde <dljtw98@gmail.com>
2024-06-22 13:59:12 +0200

    Add udf skeleton

--- src/common.cpp
+++ src/common.cpp
@@ -79,6 +79,20 @@ bool IterativeLengthFunctionData::Equals(const FunctionData &other_p) const {
   return other.csr_id == csr_id;
 }
 
+unique_ptr<FunctionData>
+LocalClusteringCoefficientFunctionData::LocalClusteringCoefficientBind(
+    ClientContext &context, ScalarFunction &bound_function,
+    vector<unique_ptr<Expression>> &arguments) {
+  if (!arguments[0]->IsFoldable()) {
+    throw InvalidInputException("Id must be constant.");
+  }
+
+  int32_t csr_id = ExpressionExecutor::EvaluateScalar(context, *arguments[0])
+                       .GetValue<int32_t>();
+
+  return make_uniq<IterativeLengthFunctionData>(context, csr_id);
+}
+
 unique_ptr<FunctionData> IterativeLengthFunctionData::IterativeLengthBind(
     ClientContext &context, ScalarFunction &bound_function,
     vector<unique_ptr<Expression>> &arguments) {
