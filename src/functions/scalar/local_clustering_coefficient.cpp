4bdfe4a04662559fe605f7de4bf315ebc53bf06f
dtenwolde <dljtw98@gmail.com>
2024-06-24 18:54:47 +0200

    Trying to get csr to be undirected

--- src/functions/scalar/local_clustering_coefficient.cpp
+++ src/functions/scalar/local_clustering_coefficient.cpp
@@ -38,7 +38,7 @@ static void LocalClusteringCoefficientFunction(DataChunk &args, ExpressionState 
   }
   int64_t *v = (int64_t *)duckpgq_state->csr_list[info.csr_id]->v;
   vector<int64_t> &e = duckpgq_state->csr_list[info.csr_id]->e;
-
+  std::cout << duckpgq_state->csr_list[info.csr_id]->ToString();
   // get src and dst vectors for searches
   auto &src = args.data[1];
   UnifiedVectorFormat vdata_src;
@@ -48,11 +48,17 @@ static void LocalClusteringCoefficientFunction(DataChunk &args, ExpressionState 
   ValidityMask &result_validity = FlatVector::Validity(result);
   // create result vector
   result.SetVectorType(VectorType::FLAT_VECTOR);
-  auto result_data = FlatVector::GetData<int64_t>(result);
+  auto result_data = FlatVector::GetData<float_t>(result);
 
   for (int32_t n = 0; n < args.size(); n++) {
-    int64_t src_node = src_data[n];
-    int64_t count = 0;
+    auto src_sel = vdata_src.sel->get_index(n);
+    int64_t src_node = src_data[src_sel];
+    int32_t number_of_edges = v[src_node + 1] - v[src_node];
+    if (number_of_edges < 2) {
+      result_data[n] = static_cast<float_t>(0.0);
+      continue;
+    }
+    int32_t count = 0;
     for (auto offset = v[src_node]; offset < v[src_node + 1]; offset++) {
       auto neighbour = e[offset];
       for (auto offset2 = v[neighbour]; offset2 < v[neighbour + 1]; offset2++) {
@@ -65,13 +71,9 @@ static void LocalClusteringCoefficientFunction(DataChunk &args, ExpressionState 
         }
       }
     }
-    int32_t number_of_edges = v[src_node + 1] - v[src_node];
-    if (number_of_edges > 1) {
-      count = count / (number_of_edges * (number_of_edges - 1));
-    } else {
-      count = 0;
-    }
-    result_data[n] = count;
+    float_t local_result =  static_cast<float_t>(count) / (number_of_edges * (number_of_edges - 1));
+    std::cout << src_node << " " << count << " " << number_of_edges << " " << local_result << std::endl;
+    result_data[n] = local_result;
   }
 
   duckpgq_state->csr_to_delete.insert(info.csr_id);
@@ -80,7 +82,7 @@ static void LocalClusteringCoefficientFunction(DataChunk &args, ExpressionState 
 CreateScalarFunctionInfo DuckPGQFunctions::GetLocalClusteringCoefficientFunction() {
   auto fun = ScalarFunction("local_clustering_coefficient",
                             {LogicalType::INTEGER, LogicalType::BIGINT},
-                            LogicalType::BIGINT, LocalClusteringCoefficientFunction,
+                            LogicalType::FLOAT, LocalClusteringCoefficientFunction,
                             LocalClusteringCoefficientFunctionData::LocalClusteringCoefficientBind);
   return CreateScalarFunctionInfo(fun);
 }
--- src/include/duckpgq/compressed_sparse_row.hpp
+++ src/include/duckpgq/compressed_sparse_row.hpp
@@ -22,5 +22,31 @@ public:
   bool initialized_w = false;
 
   size_t vsize;
+
+  string ToString() {
+    string result;
+    if (initialized_v) {
+      result += "v: ";
+      for (size_t i = 0; i < vsize; i++) {
+        result += to_string(v[i].load()) + " ";
+      }
+    }
+    result += "\n";
+    if (initialized_e) {
+      result += "e: ";
+      for (size_t i = 0; i < e.size(); i++) {
+        result += to_string(e[i]) + " ";
+      }
+    }
+    result += "\n";
+    if (initialized_w) {
+      result += "w: ";
+      for (size_t i = 0; i < w.size(); i++) {
+        result += to_string(w[i]) + " ";
+      }
+    }
+    return result;
+  };
+
 };
 } // namespace duckdb
