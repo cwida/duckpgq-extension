207c5dd0e1bbd60435b97d4dbfc6de7e667f3a63
dtenwolde <dljtw98@gmail.com>
2024-08-06 10:13:30 +0200

    Separating parser, operator and state

---
+++ src/core/operator/duckpgq_bind.cpp
@@ -0,0 +1,42 @@
+#include "duckpgq/core/operator/duckpgq_bind.hpp"
+#include "duckpgq/common.hpp"
+
+#include <duckpgq/core/parser/duckpgq_parser.hpp>
+#include "duckpgq/core/operator/duckpgq_operator.hpp"
+#include <duckpgq_state.hpp>
+
+namespace duckpgq {
+
+namespace core {
+
+BoundStatement duckpgq_bind(ClientContext &context, Binder &binder,
+                            OperatorExtensionInfo *info,
+                            SQLStatement &statement) {
+  auto lookup = context.registered_state.find("duckpgq");
+  if (lookup == context.registered_state.end()) {
+    throw; // Throw the original error that got us here if DuckPGQ is not loaded
+  }
+
+  auto duckpgq_state = (DuckPGQState *)lookup->second.get();
+  auto duckpgq_binder = Binder::CreateBinder(context, &binder);
+  auto duckpgq_parse_data =
+      dynamic_cast<DuckPGQParseData *>(duckpgq_state->parse_data.get());
+  if (duckpgq_parse_data) {
+    return duckpgq_binder->Bind(*(duckpgq_parse_data->statement));
+  }
+  throw;
+}
+
+//------------------------------------------------------------------------------
+// Register functions
+//------------------------------------------------------------------------------
+void CorePGQOperator::RegisterPGQBindOperator(
+    DatabaseInstance &db) {
+  auto &config = DBConfig::GetConfig(db);
+  config.operator_extensions.push_back(make_uniq<DuckPGQOperatorExtension>());
+
+}
+
+}
+
+} // namespace duckpgq
---
+++ src/include/duckpgq_state.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "duckpgq/common.hpp"
+
+#include <duckpgq/core/utils/compressed_sparse_row.hpp>
+
+namespace duckdb {
+
+class DuckPGQState : public ClientContextState {
+public:
+explicit DuckPGQState(unique_ptr<ParserExtensionParseData> parse_data);
+void QueryEnd() override;
+CreatePropertyGraphInfo *GetPropertyGraph(const string &pg_name);
+duckpgq::core::CSR *GetCSR(int32_t id);
+
+
+public:
+  unique_ptr<ParserExtensionParseData> parse_data;
+
+  unordered_map<int32_t, unique_ptr<ParsedExpression>> transform_expression;
+  int32_t match_index = 0;
+  int32_t unnamed_graphtable_index = 1; // Used to generate unique names for
+  // unnamed graph tables
+
+  //! Property graphs that are registered
+  std::unordered_map<string, unique_ptr<CreateInfo>> registered_property_graphs;
+
+  //! Used to build the CSR data structures required for path-finding queries
+  std::unordered_map<int32_t, unique_ptr<duckpgq::core::CSR>> csr_list;
+  std::mutex csr_lock;
+  std::unordered_set<int32_t> csr_to_delete;
+};
+
+} // namespace duckdb
