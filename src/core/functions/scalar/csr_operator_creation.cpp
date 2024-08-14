7f7793678dcf5519236ca1080cbad8cd1deb7d0d
dtenwolde <dljtw98@gmail.com>
2024-08-14 17:06:01 +0200

    Format fix

--- src/core/functions/scalar/csr_operator_creation.cpp
+++ src/core/functions/scalar/csr_operator_creation.cpp
@@ -1,27 +1,31 @@
+#include "duckdb/function/scalar_function.hpp"
 #include "duckpgq/common.hpp"
 #include <duckpgq/core/functions/function_data/csr_operator_function_data.hpp>
 #include <duckpgq/core/functions/scalar.hpp>
-#include "duckdb/function/scalar_function.hpp"
 
 namespace duckpgq {
 
 namespace core {
 
 static void CreateCSROperatorFunction(DataChunk &args, ExpressionState &state,
-                                    Vector &result) {
-  throw NotImplementedException("CSR operator creation function not implemented, should have gone to operator instead.");
+                                      Vector &result) {
+  throw NotImplementedException(
+      "CSR operator creation function not implemented, should have gone to "
+      "operator instead.");
 }
 
 //------------------------------------------------------------------------------
 // Register functions
 //------------------------------------------------------------------------------
-void CoreScalarFunctions::RegisterCSROperatorCreationScalarFunctions(DatabaseInstance &db) {
-  ExtensionUtil::RegisterFunction(db, ScalarFunction("csr_operator", {LogicalType::INTEGER, LogicalType::BIGINT,
-                                  LogicalType::BIGINT, LogicalType::BIGINT,
-                                  LogicalType::BIGINT, LogicalType::BIGINT,
-                                  LogicalType::BIGINT},
-                                  LogicalType::INTEGER, CreateCSROperatorFunction,
-                                 CSROperatorFunctionData::CSRBind));
+void CoreScalarFunctions::RegisterCSROperatorCreationScalarFunctions(
+    DatabaseInstance &db) {
+  ExtensionUtil::RegisterFunction(
+      db, ScalarFunction("csr_operator",
+                         {LogicalType::BIGINT, LogicalType::BIGINT,
+                          LogicalType::BIGINT, LogicalType::BIGINT,
+                          LogicalType::BIGINT, LogicalType::BIGINT},
+                         LogicalType::INTEGER, CreateCSROperatorFunction,
+                         CSROperatorFunctionData::CSRBind));
 }
 
 } // namespace core
