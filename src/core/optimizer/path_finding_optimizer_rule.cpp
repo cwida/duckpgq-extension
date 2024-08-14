02f4a5a69250503e0cdc276e962b946a1fb4d54c
dtenwolde <dljtw98@gmail.com>
2024-08-14 12:34:09 +0200

    Getting the option now works correctly

--- src/core/optimizer/path_finding_optimizer_rule.cpp
+++ src/core/optimizer/path_finding_optimizer_rule.cpp
@@ -19,12 +19,9 @@ namespace duckpgq {
 namespace core {
 
 bool DuckpgqOptimizerExtension::GetPathFindingOption(ClientContext &context) {
-  auto& db_config = DBConfig::GetConfig(context);
-  auto const path_finding_operator_option = db_config.options.set_variables.find("experimental_path_finding_operator");
-  if (path_finding_operator_option == db_config.options.set_variables.end()) {
-    return false; // If the path finding operator is not enabled, we do not need to do anything
-  }
-  return path_finding_operator_option->second.GetValue<bool>();
+  Value value;
+  context.TryGetCurrentSetting("experimental_path_finding_operator", value);
+  return value.GetValue<bool>();
 }
 
 bool DuckpgqOptimizerExtension::InsertPathFindingOperator(LogicalOperator &op, ClientContext &context) {
