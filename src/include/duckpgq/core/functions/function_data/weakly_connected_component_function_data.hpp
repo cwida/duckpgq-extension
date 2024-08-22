e7b89ce1c54a945090ae4c4b6046209f7056918e
dtenwolde <dljtw98@gmail.com>
2024-08-22 10:31:42 +0200

    Weakly connected component function data

---
+++ src/include/duckpgq/core/functions/function_data/weakly_connected_component_function_data.hpp
@@ -0,0 +1,35 @@
+//===----------------------------------------------------------------------===//
+//                         DuckPGQ
+//
+// duckpgq/core/functions/function_data/weakly_connected_component_function_data.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+
+#pragma once
+#include "duckdb/main/client_context.hpp"
+#include "duckpgq/common.hpp"
+
+namespace duckpgq {
+namespace core {
+struct WeaklyConnectedComponentFunctionData final : FunctionData {
+  ClientContext &context;
+  int32_t csr_id;
+  vector<int64_t> componentId;
+  std::mutex component_lock;
+  bool component_id_initialized; // if componentId is initialized
+  WeaklyConnectedComponentFunctionData(ClientContext &context, int32_t csr_id);
+  WeaklyConnectedComponentFunctionData(ClientContext &context, int32_t csr_id, const vector<int64_t> &componentId);
+  static unique_ptr<FunctionData>
+  WeaklyConnectedComponentBind(ClientContext &context, ScalarFunction &bound_function,
+                      vector<unique_ptr<Expression>> &arguments);
+
+  unique_ptr<FunctionData> Copy() const override;
+  bool Equals(const FunctionData &other_p) const override;
+};
+
+
+} // namespace core
+
+} // namespace duckpgq
