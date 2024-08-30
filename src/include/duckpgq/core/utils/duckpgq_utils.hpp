3332a06c5f0976d40f0a0e0592205ea9ac643d88
dtenwolde <dljtw98@gmail.com>
2024-08-30 10:13:32 +0200

    Add ToString method for PGQMatchType

--- src/include/duckpgq/core/utils/duckpgq_utils.hpp
+++ src/include/duckpgq/core/utils/duckpgq_utils.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "duckpgq_state.hpp"
 
+#include <duckdb/parser/path_element.hpp>
+
 namespace duckpgq {
 
 namespace core {
@@ -10,6 +12,26 @@ namespace core {
 
 // Function to get DuckPGQState from ClientContext
 DuckPGQState *GetDuckPGQState(ClientContext &context);
+
+class GraphUtils {
+public:
+  static std::string ToString(PGQMatchType matchType) {
+    switch (matchType) {
+    case PGQMatchType::MATCH_VERTEX:
+      return "MATCH_VERTEX";
+    case PGQMatchType::MATCH_EDGE_ANY:
+      return "MATCH_EDGE_ANY";
+    case PGQMatchType::MATCH_EDGE_LEFT:
+      return "MATCH_EDGE_LEFT";
+    case PGQMatchType::MATCH_EDGE_RIGHT:
+      return "MATCH_EDGE_RIGHT";
+    case PGQMatchType::MATCH_EDGE_LEFT_RIGHT:
+      return "MATCH_EDGE_LEFT_RIGHT";
+    default:
+      return "UNKNOWN_MATCH_TYPE";
+    }
+  }
+};
 
 } // namespace core
 
