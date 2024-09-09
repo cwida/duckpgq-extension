b1a1c0c08f647a31b4e257f3c6dcaa242f3abfa7
dtenwolde <dljtw98@gmail.com>
2024-09-09 16:39:46 +0200

    Update get state to use shared ptr

--- src/include/duckpgq/core/utils/duckpgq_utils.hpp
+++ src/include/duckpgq/core/utils/duckpgq_utils.hpp
@@ -9,9 +9,9 @@ namespace core {
 #define VISIT_SIZE_DIVISOR 2
 
 // Function to get DuckPGQState from ClientContext
-DuckPGQState *GetDuckPGQState(ClientContext &context);
+shared_ptr<DuckPGQState> GetDuckPGQState(ClientContext &context);
 std::string ToLowerCase(const std::string &input);
-CreatePropertyGraphInfo* GetPropertyGraphInfo(DuckPGQState *duckpgq_state, const std::string &pg_name);
+CreatePropertyGraphInfo* GetPropertyGraphInfo(shared_ptr<DuckPGQState> &duckpgq_state, const std::string &pg_name);
 shared_ptr<PropertyGraphTable> ValidateSourceNodeAndEdgeTable(CreatePropertyGraphInfo *pg_info, const std::string &node_table, const std::string &edge_table);
 unique_ptr<SelectNode> CreateSelectNode(const shared_ptr<PropertyGraphTable> &edge_pg_entry, const string& function_name, const string& function_alias);
 unique_ptr<BaseTableRef> CreateBaseTableRef(const string &table_name, const string &alias = "");
