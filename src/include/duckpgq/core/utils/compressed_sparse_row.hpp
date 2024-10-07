59dc597871f6272f203a55f816c1260720b391ed
dtenwolde <dljtw98@gmail.com>
2024-10-07 10:35:58 +0200

    Changing <> to "" for includes

--- src/include/duckpgq/core/utils/compressed_sparse_row.hpp
+++ src/include/duckpgq/core/utils/compressed_sparse_row.hpp
@@ -1,23 +1,23 @@
 //===----------------------------------------------------------------------===//
 //                         DuckPGQ
 //
-// duckpgq/utils/compressed_sparse_row.hpp
+// duckpgq/core/utils/compressed_sparse_row.hpp
 //
 //
 //===----------------------------------------------------------------------===//
 
 #pragma once
-#include <duckdb/function/function.hpp>
+#include "duckdb/function/function.hpp"
 
 #include "duckdb/parser/expression/cast_expression.hpp"
 #include "duckdb/parser/expression/function_expression.hpp"
 #include "duckdb/parser/expression/subquery_expression.hpp"
 #include "duckdb/parser/query_node/set_operation_node.hpp"
 
-#include <duckdb/parser/expression/columnref_expression.hpp>
-#include <duckdb/parser/property_graph_table.hpp>
-#include <duckdb/parser/query_node/select_node.hpp>
-#include <duckdb/parser/tableref/joinref.hpp>
+#include "duckdb/parser/expression/columnref_expression.hpp"
+#include "duckdb/parser/property_graph_table.hpp"
+#include "duckdb/parser/query_node/select_node.hpp"
+#include "duckdb/parser/tableref/joinref.hpp"
 #include "duckpgq/common.hpp"
 
 namespace duckpgq {
