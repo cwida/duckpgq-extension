031a62efa7fa7dcbd298cb932a306aa8333d328f
dtenwolde <dljtw98@gmail.com>
2024-10-07 15:52:33 +0200

    Remove unused includes

--- src/include/duckpgq_extension.hpp
+++ src/include/duckpgq_extension.hpp
@@ -1,9 +1,6 @@
 #pragma once
 
 #include "duckpgq/common.hpp"
-#include "duckdb/parser/parsed_expression.hpp"
-#include "duckpgq/core/utils/compressed_sparse_row.hpp"
-#include "duckdb/parser/parsed_data/create_property_graph_info.hpp"
 
 namespace duckdb {
 
