72bcfd86eebc981365a19d3742a1f1c091997720
dtenwolde <dljtw98@gmail.com>
2024-10-29 13:08:37 +0100

    Improved loading extension whenever a connection opens

--- src/include/duckpgq_extension.hpp
+++ src/include/duckpgq_extension.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
 #include "duckpgq/common.hpp"
-#include "duckdb/planner/extension_callback.hpp"
-#include <duckpgq_state.hpp>
 
 namespace duckdb {
 
@@ -10,18 +8,6 @@ class DuckpgqExtension : public Extension {
 public:
   void Load(DuckDB &db) override;
   std::string Name() override;
-};
-
-class DuckpgqLoadExtension : public ExtensionCallback {
-  void OnConnectionOpened(ClientContext &context) override {
-    auto duckpgq_state = context.registered_state->Get<DuckPGQState>("duckpgq");
-    if (duckpgq_state == nullptr) {
-      auto state = make_shared_ptr<DuckPGQState>();
-      context.registered_state->Insert("duckpgq", state);
-      std::cout << "initialized duckpgq state" << std::endl;
-    }
-
-  }
 };
 
 } // namespace duckpgq
---
+++ src/include/duckpgq_extension_callback.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include "duckpgq/common.hpp"
+#include "duckdb/planner/extension_callback.hpp"
+#include <duckpgq_state.hpp>
+
+namespace duckdb {
+class DuckpgqExtensionCallback : public ExtensionCallback {
+  void OnConnectionOpened(ClientContext &context) override {
+    context.registered_state->Insert("duckpgq", make_shared_ptr<DuckPGQState>());
+  };
+};
+}
