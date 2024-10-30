3cc2bf392c18fe162589642c051c000f9ed3bafe
dtenwolde <dljtw98@gmail.com>
2024-10-30 11:39:34 +0100

    Format fix

--- src/duckpgq_state.cpp
+++ src/duckpgq_state.cpp
@@ -5,22 +5,22 @@ namespace duckdb {
 DuckPGQState::DuckPGQState(shared_ptr<ClientContext> context) {
   auto new_conn = make_shared_ptr<ClientContext>(context->db);
   auto query = new_conn->Query("CREATE TABLE IF NOT EXISTS __duckpgq_internal ("
-                  "property_graph varchar, "
-                  "table_name varchar, "
-                  "label varchar, "
-                  "is_vertex_table boolean, "
-                  "source_table varchar, "
-                  "source_pk varchar[], "
-                  "source_fk varchar[], "
-                  "destination_table varchar, "
-                  "destination_pk varchar[], "
-                  "destination_fk varchar[], "
-                  "discriminator varchar, "
-                  "sub_labels varchar[])", false);
+                               "property_graph varchar, "
+                               "table_name varchar, "
+                               "label varchar, "
+                               "is_vertex_table boolean, "
+                               "source_table varchar, "
+                               "source_pk varchar[], "
+                               "source_fk varchar[], "
+                               "destination_table varchar, "
+                               "destination_pk varchar[], "
+                               "destination_fk varchar[], "
+                               "discriminator varchar, "
+                               "sub_labels varchar[])",
+                               false);
   if (query->HasError()) {
     throw TransactionException(query->GetError());
   }
-
 
   RetrievePropertyGraphs(new_conn);
 }
@@ -45,20 +45,20 @@ void DuckPGQState::RetrievePropertyGraphs(shared_ptr<ClientContext> context) {
       // Handle edge table related things.
       table->source_reference = chunk->GetValue(4, i).GetValue<string>();
       auto source_pk_chunk = ListValue::GetChildren(chunk->GetValue(5, i));
-      for (const auto& source_pk: source_pk_chunk) {
+      for (const auto &source_pk : source_pk_chunk) {
         table->source_pk.push_back(source_pk.GetValue<string>());
       }
       auto source_fk_chunk = ListValue::GetChildren(chunk->GetValue(6, i));
-      for (const auto& source_fk: source_fk_chunk) {
+      for (const auto &source_fk : source_fk_chunk) {
         table->source_fk.push_back(source_fk.GetValue<string>());
       }
       table->destination_reference = chunk->GetValue(7, i).GetValue<string>();
       auto destination_pk_chunk = ListValue::GetChildren(chunk->GetValue(8, i));
-      for (const auto& dest_pk: destination_pk_chunk) {
+      for (const auto &dest_pk : destination_pk_chunk) {
         table->destination_pk.push_back(dest_pk.GetValue<string>());
       }
       auto destination_fk_chunk = ListValue::GetChildren(chunk->GetValue(9, i));
-      for (const auto& dest_fk: destination_fk_chunk) {
+      for (const auto &dest_fk : destination_fk_chunk) {
         table->destination_fk.push_back(dest_fk.GetValue<string>());
       }
     }
@@ -66,15 +66,18 @@ void DuckPGQState::RetrievePropertyGraphs(shared_ptr<ClientContext> context) {
     if (discriminator != "NULL") {
       table->discriminator = discriminator;
       auto sublabels = ListValue::GetChildren(chunk->GetValue(11, i));
-      for (const auto& sublabel : sublabels) {
+      for (const auto &sublabel : sublabels) {
         table->sub_labels.push_back(sublabel.GetValue<string>());
       }
     }
 
-    if (registered_property_graphs.find(property_graph_name) == registered_property_graphs.end()) {
-      registered_property_graphs[property_graph_name] = make_uniq<CreatePropertyGraphInfo>(property_graph_name);
+    if (registered_property_graphs.find(property_graph_name) ==
+        registered_property_graphs.end()) {
+      registered_property_graphs[property_graph_name] =
+          make_uniq<CreatePropertyGraphInfo>(property_graph_name);
     }
-    auto &pg_info = registered_property_graphs[property_graph_name]->Cast<CreatePropertyGraphInfo>();
+    auto &pg_info = registered_property_graphs[property_graph_name]
+                        ->Cast<CreatePropertyGraphInfo>();
     pg_info.label_map[table->main_label] = table;
     if (!table->discriminator.empty()) {
       for (const auto &label : table->sub_labels) {
@@ -115,6 +118,5 @@ duckpgq::core::CSR *DuckPGQState::GetCSR(int32_t id) {
   }
   return csr_entry->second.get();
 }
-
 
 } // namespace duckdb
