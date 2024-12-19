d639d81ff84ef303d7db6e314b7bbd6ac44d1f87
dtenwolde <dljtw98@gmail.com>
2024-12-19 20:27:00 +0100

    Add pragma show_property_graphs

---
+++ src/core/pragma/show_property_graphs.cpp
@@ -0,0 +1,27 @@
+#include "duckdb/function/pragma_function.hpp"
+#include "duckdb/main/extension_util.hpp"
+#include <duckpgq/core/pragma/duckpgq_pragma.hpp>
+
+namespace duckpgq {
+
+namespace core {
+
+static string PragmaShowPropertyGraphs(ClientContext &context, const FunctionParameters &parameters) {
+  return "SELECT DISTINCT property_graph from __duckpgq_internal";
+}
+
+void CorePGQPragma::RegisterShowPropertyGraphs(DatabaseInstance &instance) {
+  // Define the pragma function
+  auto pragma_func = PragmaFunction::PragmaCall(
+                  "show_property_graphs",                 // Name of the pragma
+                  PragmaShowPropertyGraphs,         // Query substitution function
+                  {}           // Parameter types (mail_limit is an integer)
+  );
+
+  // Register the pragma function
+  ExtensionUtil::RegisterFunction(instance, pragma_func);
+}
+
+} // namespace core
+
+} // namespace duckpgq
