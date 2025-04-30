244924df419db2ae4543d806d48d743ad54fa337
dtenwolde <dljtw98@gmail.com>
2025-04-30 11:24:54 +0200

    Started on bindreplace for summarize

--- src/core/functions/table/summarize_property_graph.cpp
+++ src/core/functions/table/summarize_property_graph.cpp
@@ -1,62 +1,65 @@
 #include "duckpgq/core/functions/table/summarize_property_graph.hpp"
-#include "duckdb/parser/parsed_data/create_property_graph_info.hpp"
 #include "duckdb/parser/query_node/select_node.hpp"
-#include "duckdb/parser/statement/create_statement.hpp"
 #include "duckdb/parser/tableref/showref.hpp"
 #include <duckpgq/core/functions/table.hpp>
-#include <duckpgq/core/parser/duckpgq_parser.hpp>
 #include <duckpgq/core/utils/duckpgq_utils.hpp>
-#include <duckpgq_extension.hpp>
+#include "duckdb/parser/tableref/subqueryref.hpp"
+#include "duckdb/parser/statement/select_statement.hpp"
+#include "duckdb/parser/expression/constant_expression.hpp"
+#include "duckdb/parser/expression/star_expression.hpp"
+#include "duckdb/parser/tableref/emptytableref.hpp"
 
 namespace duckpgq {
 namespace core {
 
-unique_ptr<FunctionData>
-SummarizePropertyGraphFunction::SummarizePropertyGraphBind(
-    ClientContext &context, TableFunctionBindInput &input,
-    vector<LogicalType> &return_types, vector<string> &names) {
+unique_ptr<ParsedExpression> GetTableNameConstantExpression(PropertyGraphTable &pg_table) {
+  auto table_name_column = make_uniq<ConstantExpression>(Value(pg_table.table_name));
+  table_name_column->alias = "table_name";
+  return table_name_column;
+}
+
+
+unique_ptr<CommonTableExpressionInfo> SummarizePropertyGraphFunction::CreateVertexTableCTE(shared_ptr<PropertyGraphTable> &vertex_table) {
+  auto cte_info = make_uniq<CommonTableExpressionInfo>();
+  auto select_statement = make_uniq<SelectStatement>();
+  auto select_node = make_uniq<SelectNode>();
+  select_node->select_list.push_back(GetTableNameConstantExpression(*vertex_table));
+  select_node->select_list.push_back(make_uniq<ConstantExpression>(Value())); // source table name (NULL for vertex tables)
+  select_node->select_list.push_back(make_uniq<ConstantExpression>(Value())); // destination table name (NULL for vertex tables)
+
+  select_node->from_table = make_uniq<BaseTableRef>(TableDescription(vertex_table->catalog_name, vertex_table->schema_name, vertex_table->table_name));
+  select_statement->node = std::move(select_node);
+  cte_info->query = std::move(select_statement);
+  return cte_info;
+}
+
+unique_ptr<TableRef> SummarizePropertyGraphFunction::SummarizePropertyGraphBindReplace(
+  ClientContext &context,
+  TableFunctionBindInput &bind_input) {
   auto duckpgq_state = GetDuckPGQState(context);
 
-  const auto duckpgq_parse_data =
-      dynamic_cast<DuckPGQParseData *>(duckpgq_state->parse_data.get());
-
-  if (!duckpgq_parse_data) {
-    return {};
+  string property_graph = bind_input.inputs[0].GetValue<string>();
+  auto pg_info = duckpgq_state->GetPropertyGraph(property_graph);
+  auto select_node = make_uniq<SelectNode>();
+  for (auto &vertex_table : pg_info->vertex_tables) {
+    select_node->cte_map.map.insert(vertex_table->table_name + "_stats", CreateVertexTableCTE(vertex_table));
+    auto base_table_ref = make_uniq<BaseTableRef>();
+    base_table_ref->table_name = vertex_table->table_name + "_stats";
+    select_node->from_table = std::move(base_table_ref);
   }
-  auto statement =
-      dynamic_cast<SelectStatement *>(duckpgq_parse_data->statement.get());
-  auto select_node = dynamic_cast<SelectNode *>(statement->node.get());
-  auto show_ref = dynamic_cast<ShowRef *>(select_node->from_table.get());
 
-  auto pg_table =
-      duckpgq_state->registered_property_graphs.find(show_ref->table_name);
-  if (pg_table == duckpgq_state->registered_property_graphs.end()) {
-    throw Exception(ExceptionType::INVALID, "Property graph " +
-                                                show_ref->table_name +
-                                                " does not exist.");
-  }
-  auto property_graph =
-      dynamic_cast<CreatePropertyGraphInfo *>(pg_table->second.get());
+  select_node->select_list.push_back(make_uniq<StarExpression>());
 
-  return make_uniq<SummarizePropertyGraphBindData>(property_graph);
+  auto select_stmt = make_uniq<SelectStatement>();
+  select_stmt->node = std::move(select_node);
+  auto subquery = make_uniq<SubqueryRef>(std::move(select_stmt));
+  subquery->Print();
+
+
+  return std::move(subquery);
 }
 
-unique_ptr<GlobalTableFunctionState>
-SummarizePropertyGraphFunction::SummarizePropertyGraphInit(
-    ClientContext &context, TableFunctionInitInput &input) {
-  return make_uniq<SummarizePropertyGraphGlobalData>();
-}
 
-void SummarizePropertyGraphFunction::SummarizePropertyGraphFunc(
-    ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
-  auto &bind_data = data_p.bind_data->Cast<SummarizePropertyGraphBindData>();
-  auto &data = data_p.global_state->Cast<SummarizePropertyGraphGlobalData>();
-  if (data.done) {
-    return;
-  }
-  auto pg_info = bind_data.summarize_pg_info;
-  data.done = true;
-}
 
 //------------------------------------------------------------------------------
 // Register functions
