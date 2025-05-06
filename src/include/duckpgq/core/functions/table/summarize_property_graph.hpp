0f9c6b54e7fea63996c40eeab383e945ab892516
dtenwolde <dljtw98@gmail.com>
2025-05-06 09:20:06 +0200

    Format fix this one as well

--- src/include/duckpgq/core/functions/table/summarize_property_graph.hpp
+++ src/include/duckpgq/core/functions/table/summarize_property_graph.hpp
@@ -7,9 +7,9 @@
 //===----------------------------------------------------------------------===//
 
 #pragma once
-#include "duckpgq/common.hpp"
 #include "duckdb/function/table_function.hpp"
 #include "duckdb/parser/parsed_data/create_property_graph_info.hpp"
+#include "duckpgq/common.hpp"
 
 namespace duckpgq {
 
@@ -38,18 +38,32 @@ public:
   SummarizePropertyGraphInit(ClientContext &context,
                              TableFunctionInitInput &input);
 
-  static unique_ptr<SubqueryRef> CreateGroupBySubquery(const shared_ptr<PropertyGraphTable> &pg_table, bool is_in_degree, const string &degree_column);
-  static unique_ptr<ParsedExpression> GetDegreeStatistics(const string &aggregate_function, bool is_in_degree);
-  static unique_ptr<CommonTableExpressionInfo> CreateDegreeStatisticsCTE(const shared_ptr<PropertyGraphTable> &pg_table, const string &degree_column, bool is_in_degree);
-  static unique_ptr<ParsedExpression> GetIsolatedNodes(shared_ptr<PropertyGraphTable> &pg_table, const string &alias, bool is_source);
-  static unique_ptr<ParsedExpression> GetDistinctCount(const shared_ptr<PropertyGraphTable> &pg_table, const string &alias, bool is_source);
+  static unique_ptr<SubqueryRef>
+  CreateGroupBySubquery(const shared_ptr<PropertyGraphTable> &pg_table,
+                        bool is_in_degree, const string &degree_column);
+  static unique_ptr<ParsedExpression>
+  GetDegreeStatistics(const string &aggregate_function, bool is_in_degree);
+  static unique_ptr<CommonTableExpressionInfo>
+  CreateDegreeStatisticsCTE(const shared_ptr<PropertyGraphTable> &pg_table,
+                            const string &degree_column, bool is_in_degree);
+  static unique_ptr<ParsedExpression>
+  GetIsolatedNodes(shared_ptr<PropertyGraphTable> &pg_table,
+                   const string &alias, bool is_source);
+  static unique_ptr<ParsedExpression>
+  GetDistinctCount(const shared_ptr<PropertyGraphTable> &pg_table,
+                   const string &alias, bool is_source);
 
-  static unique_ptr<CommonTableExpressionInfo> CreateVertexTableCTE(const shared_ptr<PropertyGraphTable> &vertex_table);
-  static unique_ptr<CommonTableExpressionInfo> CreateEdgeTableCTE(shared_ptr<PropertyGraphTable> &edge_table);
+  static unique_ptr<CommonTableExpressionInfo>
+  CreateVertexTableCTE(const shared_ptr<PropertyGraphTable> &vertex_table);
+  static unique_ptr<CommonTableExpressionInfo>
+  CreateEdgeTableCTE(shared_ptr<PropertyGraphTable> &edge_table);
 
-  static unique_ptr<TableRef> HandleSingleVertexTable(const shared_ptr<PropertyGraphTable> &vertex_table, const string &stat_table_alias);
-  static unique_ptr<TableRef> SummarizePropertyGraphBindReplace(ClientContext &context,
-                                        TableFunctionBindInput &input);
+  static unique_ptr<TableRef>
+  HandleSingleVertexTable(const shared_ptr<PropertyGraphTable> &vertex_table,
+                          const string &stat_table_alias);
+  static unique_ptr<TableRef>
+  SummarizePropertyGraphBindReplace(ClientContext &context,
+                                    TableFunctionBindInput &input);
 };
 
 } // namespace core
